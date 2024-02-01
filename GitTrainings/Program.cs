ebe7489633d6b8faf8bc106bf079ab24a401ddb7
Artem Ilchenko <artilc@auditdata.com>
2024-02-01 18:53:11 +0200

    Add project files.

---
+++ GitTrainings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTrainings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
